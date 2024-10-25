cecc81a350ec93d75db8942c741dd85e7a1c5122
변영화[주문&결제FE] <younghwa.byun@navercorp.com>
2024-10-25 14:43:01 +0900

    테스슽

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 /** @see https://ko.vitejs.dev/config/ */
 import type {UserConfig} from 'vite'
-import {getBabelOutputPlugin} from '@rollup/plugin-babel'
+import {getBabelOutputPlugin, babel} from '@rollup/plugin-babel'
 import dts from 'vite-plugin-dts'
+import pkg from './package.json'
 
 export default {
   build: {
+    // 트랜스파일은 esbuild가 하고
+    // 폴리필은 babel이 해줘
+
+    // 라이브러리는 runtime
+    //
+
+    // 변수 타입을 모르니깐 이름 같은 폴리필 넣음;
+    // 이름에 예약어 넣지마?
+
+    target: 'chrome120',
     // 라이브러리 모드
     lib: {
       // 진입 파일
@@ -13,26 +24,11 @@ export default {
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      plugins: [
-        getBabelOutputPlugin({
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                // `targets`을 설정하지 않으면 `.browserslistrc`를 읽는다.
-                targets: undefined,
-                // 폴리필 처리 최적화
-                // 필요한 폴리필만 자동으로 포함시킨다.
-                useBuiltIns: 'usage',
-                // babel에게 어떤 corejs 버전을 쓸건지 알려주기
-                corejs: '3.38',
-                // targets이 뭔지 출력하기
-                debug: true,
-              },
-            ],
-          ],
-        }),
-      ],
+      external: [
+        // ...Object.keys(pkg.peerDependencies),
+        ...Object.keys(pkg.dependencies),
+      ].flatMap((dep) => [dep, new RegExp(`^${dep}/.*`)]),
+
       output: [
         {
           dir: 'dist',
@@ -56,6 +52,20 @@ export default {
       insertTypesEntry: true,
       // 선언 파일을 index.d.ts 하나로 안묶고 배럴 파일만..
       rollupTypes: false,
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            corejs: {version: 3, proposals: true},
+            debug: true,
+          },
+        ],
+      ],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      exclude: 'node_modules/**',
     }),
   ],
 } satisfies UserConfig
